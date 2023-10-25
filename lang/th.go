26295bd7e3fe681fcc0d889cd490b009dcd5ee80
ttoonn112 <ttoonn112@gmail.com>
2023-10-25 12:31:51 +0700

    Update th.go

--- lang/th.go
+++ lang/th.go
@@ -10,7 +10,7 @@ var TH map[string]string = map[string]string{
   "error.ContactAdmin" : "ไม่สามารถดำเนินการได้ กรุณาติดต่อผู้ดูแลระบบ",
 
   "require.Id" : "ไม่พบข้อมูล ID",
-  "require.Name" : "กรุณาระบุชื่อ",
+  "require.Name" : "กรุณาระบุชื่อ-นามสกุล",
   "require.Username" : "กรุณาระบุชื่อผู้ใช้งานหรือเบอร์โทร",
   "require.Phone" : "กรุณาระบุเบอร์โทร",
   "require.Password" : "กรุณาระบุรหัสผ่าน",
